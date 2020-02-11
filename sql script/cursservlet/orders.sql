72210331a0ce6c8976e5ea19e569dfa576c035a9
Marian Mihai <mihai_ionut97@yahoo.com>
2020-02-11 12:30:21 +0200

    change table name

--- sql script/cursservlet/orders.sql
+++ sql script/cursservlet/orders.sql
@@ -1,26 +1,26 @@
 use `internship`;
 
-drop table if exists orders;
-drop table if exists ordersinfo;
+drop table if exists `order`;
+drop table if exists `orderinfo`;
 
-create table orders(
+create table `order`(
 `id` int primary key not null auto_increment,
 `owner` varchar(20) not null,
 `orderDate` date not null,
 `expDate` date not null,
-constraint F_Key_Orders foreign key (`owner`) references users (`username`)
+constraint F_Key_Orders foreign key (`owner`) references `user` (`username`)
 );
 
-insert into orders(`owner`, `orderDate`, `expDate`) 
+insert into `order`(`owner`, `orderDate`, `expDate`) 
 values ('mihai', curdate(), curdate());
 
-create table ordersinfo(
+create table `orderinfo`(
 `id` int primary key not null auto_increment,
 `orderId` int not null,
 `product` varchar(20) not null,
-constraint F_Key_OrderInfoId foreign key (`orderId`) references orders (`id`),
+constraint F_Key_OrderInfoId foreign key (`orderId`) references `order` (`id`),
 constraint F_Key_OrderInfoProd foreign key (`product`) references product (`pName`)
 );
 
-insert into ordersinfo(`orderId`, `product`) values (1, 'Dacia');
+insert into orderinfo(`orderId`, `product`) values (1, 'Dacia');
 
--- sql script/cursservlet/user-app-script.sql
+++ sql script/cursservlet/user-app-script.sql
@@ -1,18 +1,18 @@
 use `internship`;
 
-drop table if exists internship.users;
+drop table if exists internship.user;
 
-create table users(
+create table `user`(
 `username` varchar(20) not null,
 `password` varchar(80) not null,
 constraint PK_user primary key (username)
 );
 
-insert into internship.users (username, password) values ('mihai',
+insert into internship.`user` (username, password) values ('mihai',
  '$2a$10$ZyYIZn6yS8qzNVBJPJGTbOjzR4j/z6j4JY8oinVJjEYROsw.MUwX.'); # mihai parola
-insert into internship.users (username, password) values ('ana',
+insert into internship.`user` (username, password) values ('ana',
  '$2a$10$V76UeyA9HUjPU0ermY5X6OnguD7t2n7fpenzZmIvwJ7hgi2d56w6.'); # ana parola
-insert into internship.users (username, password) values ('user',
+insert into internship.`user` (username, password) values ('user',
  '$2a$10$K3CgrVoOanitFrlt0u96R.st0j.s8G/u/JioiWzkHNLkyaqY7sTxy'); # user parola
 
 # pentru parole https://www.browserling.com/tools/bcrypt
